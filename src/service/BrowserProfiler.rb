b0553324999fbb267719ea85176361c3f12c7220
Steven Spencer <steve@bigfrog.net>
2008-12-13 00:41:04 -0800

    Added new "sample" method to BrowserProfiler service. At most 1000 samples are taken. Samples are either returned immediately through a callback, or stored in an array and sent after the call to stop() or as a result of the completion of start(). Added example HTML file. Added small Ruby test file than can be run from command line to test changes.

--- src/service/BrowserProfiler.rb
+++ src/service/BrowserProfiler.rb
@@ -1,41 +1,137 @@
+#
+# Profile the current browser (currently hacked )
+# 
 class BrowserProfiler
+
+  # Reasonable number of samples, so we don't use too much memory
+  @@MAX_SAMPLES = 1000
+
+  # Constructor
   def initialize(args)
     @takingSamples = true
+    @sampleList = []
+    @maxSamples = 0
+    @sampleNumber = 0
   end
-  
-  def start(bp, args)
 
-    if (args['interval'] < 0.1)
-      args['interval'] = 0.1
+  #
+  # Start the sampling process.  At most 1,000 samples are taken.  If no callback
+  # is provided, the samples are stored and return after this method completes
+  # or 1,000 samples are taken.
+  #
+  def start(bp, args)
+    interval = args['interval'] || 1.0
+    if (interval < 0.1)
+      interval = 0.1
     end
-    
+
     @takingSamples = true
-    Thread.new(bp, args['callback'], args['interval']) do |bp,callback,interval |
+    @maxSamples = @@MAX_SAMPLES
+    @sampleList = []
+    @sampleNumber = 0
+    Thread.new(bp, args['callback'], interval) do | bp,callback,interval |
       while @takingSamples
-        x = `ps -ocomm,pcpu,rss -xwwc | grep firefox-bin | head -1`
-        cpu = /\d+\.\d+/.match(x)[0]
-        rss = /\d+$/.match(x)[0]
-        callback.invoke({'cpu' => Float(cpu), 'memory' => Integer(rss) * 1024})
-        sleep interval
+        @sampleNumber += 1
+        s = _get_sample()
+        if (callback)
+          callback.invoke(s)
+        else
+          @sampleList.push(s)
+        end
+
+        @maxSamples -= 1
+        if @maxSamples > 0
+          sleep interval
+        else
+          break
+        end
+
       end
-      bp.complete()
+      bp.complete(@sampleList)
     end
   end
-  
+
+  #
+  # Take 1 sample.
+  #
+  def sample(bp, args)
+    bp.complete(_get_sample())
+  end
+
+  #
+  # Stop the sampling.  Return the sampleList if no callback was provided to start.
+  #
   def stop(bp, args)
     @takingSamples = false
-    bp.complete()
+    bp.complete(@sampleList)
   end
 
+  #
+  # Get sample data for the given browser.
+  #
+  def _ps_browser(pat)
+    x = `ps -ocomm,pcpu,rss -xwwc | grep -i #{pat}`
+
+    if x.length > 0
+      cpu = Float(/\d+\.\d+/.match(x)[0])
+      mem = Float(/\d+$/.match(x)[0])*1024.0
+    else
+      cpu = -1.0
+      mem = -1.0
+    end
+    
+    return [cpu, mem]
+  end
+  
+  #
+  # Get a single sample.  Three system calls are made, 1 to iostat and 2 to ps
+  #
+  def _get_sample
+    # problem passing this thru to javascript as a float
+    #stime = Time.now.to_f
+    stime = Time.now.to_s
+    
+    x = `iostat -n 0 | tail -1`
+    x = x.scan(/\d+/)
+    user = Float(x[0])
+    sys  = Float(x[1])
+
+    x = _ps_browser("firefox-bin")
+    fcpu = x[0]
+    fmem = x[1]
+
+    x = _ps_browser("safari")
+    scpu = x[0]
+    smem = x[1]
+
+    return {'sample' => @sampleNumber, 'time' => stime, 'ffxcpu' => fcpu, 'ffxmem' =>  fmem, 'safcpu' => scpu, 'safmem' => smem, 'sys'=>sys, 'user'=>user}
+
+  end
 end
 
 rubyCoreletDefinition = {
   'class' => "BrowserProfiler",
-  'name' => "BrowserProfiler",
+  'name'  => "BrowserProfiler",
   'major_version' => 0,
   'minor_version' => 0,
-  'micro_version' => 2,
-  'documentation' => 'A service that analyzes the memory and cpu usage of a webbrowser.',
+  'micro_version' => 3,
+  'documentation' => 
+    'A service that analyzes the memory and cpu usage of a web browser.  ' +
+    'The service can take 1 sample or multiple samples at a specified interval.  ' +
+    'When sampling at intervals, at most 1,000 samples are taken.  If you provide ' + 
+    'a callback function, your javascript will be called after every sample is taken.  ' + 
+    'If no callback is provided, all samples are stored in an array and returned after start() ' + 
+    'completes or stop() is called.\n' + 
+    'The sample object is a map with the following keys (most values are floats):\n' +
+    '[sample] - the sample number (1-1,000)\n' + 
+    '[time]   - the string representation of the time the sample was taken\n' + 
+    '[sys]    - the percentage CPU "sys" processes are using\n' + 
+    '[user]   - the percentage CPU "user" processes are using\n' + 
+    '[ffxcpu] - the percentage CPU Firefox is using, or -1.0 if it is not running\n' +
+    '[ffxmem] - the amout of memory Firefox is using, or -1.0 if it is not running\n' +
+    '[safcpu] - the percentage CPU Safari is using, or -1.0 if it is not running\n' +
+    '[safmem] - the amout of memory Safari is using, or -1.0 if it is not running\n',
+
   'functions' =>
   [
     {
@@ -52,7 +148,7 @@ rubyCoreletDefinition = {
           {
             'name' => 'callback',
             'type' => 'callback',
-            'required' => true,
+            'required' => false,
             'documentation' => 'the callback to send a hello message to'
           }
       ]
@@ -60,8 +156,14 @@ rubyCoreletDefinition = {
 
     {
       'name' => 'stop',
-      'documentation' => "Stop taking samples.",
-      'arguments' =>[]
-    }  
+      'documentation' => "Stop taking samples.  Passes back the array of all samples.",
+      'arguments' => []
+    },
+    
+    {
+      'name' => 'sample',
+      'documentation' => "Returns a single sample, map of {memory, cpu}",
+      'arguments' => []
+    }
   ] 
 }
